e3a8a0dbe4eda9ecd4cc7708256aa64fc5e00b48
Yuri Braga <yuricancelabraga@gmail.com>
2025-09-30 17:51:17 -0300

    Products crud

--- CleanArchMvc.WebUI/Controllers/CategoriesController.cs
+++ CleanArchMvc.WebUI/Controllers/CategoriesController.cs
@@ -1,3 +1,4 @@
+using CleanArchMvc.Application.DTOs;
 using CleanArchMvc.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -21,4 +22,79 @@ public class CategoriesController : Controller
     return View(categories);
   }
 
+  [HttpGet]
+  public IActionResult Create()
+  {
+    return View();
+  }
+
+  [HttpPost]
+  public async Task<IActionResult> Create(CategoryDTO category)
+  {
+    if (ModelState.IsValid)
+    {
+      await _categoryService.Add(category);
+      return RedirectToAction(nameof(Index));
+    }
+    return View(category);
+  }
+
+  [HttpGet]
+  public async Task<IActionResult> Edit(int? id)
+  {
+    if (id == null) return NotFound();
+
+    var category = await _categoryService.GetById(id);
+    if (category == null) return NotFound();
+
+    return View(category);
+  }
+
+  [HttpPost]
+  public async Task<IActionResult> Edit(CategoryDTO categoryDTO)
+  {
+    if (ModelState.IsValid)
+    {
+      try
+      {
+        await _categoryService.Update(categoryDTO);
+      }
+      catch (Exception)
+      {
+        throw;
+      }
+      return RedirectToAction(nameof(Index));
+    }
+    return View(categoryDTO);
+  }
+
+  [HttpGet]
+  public async Task<IActionResult> Delete(int? id)
+  {
+    if (id == null) return NotFound();
+
+    var category = await _categoryService.GetById(id);
+
+    if (category == null) return NotFound();
+
+    return View(category);
+  }
+
+  [HttpPost, ActionName("Delete")]
+  public async Task<IActionResult> DeleteConfirmed(int id)
+  {
+    await _categoryService.Remove(id);
+    return RedirectToAction(nameof(Index));
+  }
+
+  [HttpGet]
+  public async Task<IActionResult> Details(int? id)
+  {
+    if (id == null) return NotFound();
+
+    var category = await _categoryService.GetById(id);
+    if (category == null) return NotFound();
+
+    return View(category);
+  }
 }
--- CleanArchMvc.WebUI/Controllers/ProductsController.cs
+++ CleanArchMvc.WebUI/Controllers/ProductsController.cs
@@ -1,18 +1,21 @@
+using CleanArchMvc.Application.DTOs;
 using CleanArchMvc.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CleanArchMvc.WebUI.Controllers;
 
 public class ProductsController : Controller
 {
   private readonly IProductService _productsService;
+  private readonly ICategoryService _categoryService;
 
-  public ProductsController(IProductService productService)
+  public ProductsController(IProductService productService, ICategoryService categoryService)
   {
     _productsService = productService;
+    _categoryService = categoryService;
   }
 
-  // GET: ProductsController
   [HttpGet]
   public async Task<IActionResult> Index()
   {
@@ -20,4 +23,81 @@ public class ProductsController : Controller
     return View(products);
   }
 
+  [HttpGet]
+  public async Task<IActionResult> Create()
+  {
+    ViewBag.Categories = new SelectList(await _categoryService.GetCategories(), "Id", "Name");
+    return View();
+  }
+
+  [HttpPost]
+  public async Task<IActionResult> Create(ProductDTO productDto)
+  {
+    if (ModelState.IsValid)
+    {
+      await _productsService.Add(productDto);
+      return RedirectToAction(nameof(Index));
+    }
+    return View(productDto);
+  }
+
+  [HttpGet]
+  public async Task<IActionResult> Edit(int? id)
+  {
+    if (id == null) return NotFound();
+    var product = await _productsService.GetById(id);
+
+    if (product == null) return NotFound();
+
+    ViewBag.Categories = new SelectList(await _categoryService.GetCategories(), "Id", "Name", product.CategoryId);
+
+    return View(product);
+  }
+
+  [HttpPost]
+  public async Task<IActionResult> Edit(ProductDTO productDto)
+  {
+    if (ModelState.IsValid)
+    {
+      try
+      {
+        await _productsService.Update(productDto);
+        return RedirectToAction(nameof(Index));
+      }
+      catch
+      {
+        return View(productDto);
+      }
+    }
+    return View(productDto);
+  }
+
+  [HttpGet]
+  public async Task<IActionResult> Delete(int? id)
+  {
+    if (id == null) return NotFound();
+
+    var product = await _productsService.GetById(id);
+    if (product == null) return NotFound();
+
+    return View(product);
+  }
+
+  [HttpPost, ActionName("Delete")]
+  public async Task<IActionResult> DeleteConfirmed(int id)
+  {
+    await _productsService.Remove(id);
+    return RedirectToAction(nameof(Index));
+  }
+
+  [HttpGet]
+  public async Task<IActionResult> Details(int? id)
+  {
+    if (id == null) return NotFound();
+
+    var product = await _productsService.GetById(id);
+    if (product == null) return NotFound();
+
+    return View(product);
+  }
 }
